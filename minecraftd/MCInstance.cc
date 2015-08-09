9030a43120747b2c2ef5108662d090759a88bafd
E. Mark Anderson <kd0bpv@gmail.com>
2015-08-08 20:44:11 -0500

    Cleanups. Make fields Atomic for thread-safety. More implementation work.

--- minecraftd/MCInstance.cc
+++ minecraftd/MCInstance.cc
@@ -1,16 +1,16 @@
 /*
  * minecraftd/MCInstance.cc
- * 
+ *
  * Server instance management - Implementation
- * 
+ *
  * Author: E. Mark Anderson <kd0bpv@gmail.com>
- * 
+ *
  * This file implements the Server Instance Management system which shall
  * be responsible for monitoring and controlling individual Server.jar
  * instances, and handle I/O to and from said instance.
- * 
+ *
  * Copyright (C) 2014-2015 E. Mark Anderson
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -21,49 +21,65 @@
  * GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */ 
+ */
 
-#include <thread>
-#include <string>
-/* fstream derives from iostream, giving us both. :) */
-#include <fstream>
-
-#include "lib/event/libevent.hh"
 #include "MCInstance.hh"
 
 MCInstance::MCInstance()
 {
 	/* Initialize fields */
-	this->failCount = 0;
-	this->isModded = false;
-	this->state = State::STOPPED;
-	
-	/* Register our own event handlers */
-	this->onFail.register_handler([this] (const Event * ev) {
-		this->failCount++;
-		if (this->failCount >= FAIL_LIMIT || ev->abort == true)
-		{
-			this->state = State::FAILED;
-		}
-	});
+	failCount = 0;
+	state = State::STOPPED;
 }
 
-void MCInstance::resetFailState()
+void MCInstance::resetFailState() noexcept
 {
-	this->failCount = 0;
-	this->state = State::STOPPED;
+	failCount = 0;
+	state = State::STOPPED;
 }
 
-void MCInstance::stop(bool force = false)
+bool MCInstance::stop(bool force)
 {
+	if (state == State::STOPPING || state == State::STOPPED ||
+		!runner.joinable())
+		return false;
+
 	if (force == true) {
 		/* Kill the process and declare failure. */
-		
-		
-		
-		this->onFail.abort = true;
-		this->onFail.fire();
+
+
+
+		FailEvent.fire(this);
 	} else {
-		
+		/* Close the process cleanly */
+
 	}
+
+	runner.join();
+	return true;
+}
+
+bool MCInstance::start()
+{
+	if (runner.joinable() || state == State::STARTING ||
+		state == State::RUNNING)
+		return false;
+
+	runner = new std::thread (&MCInstance::run(), this);
+	return true;
+}
+
+void MCInstance::run()
+{
+
+}
+
+void MCInstance::sendRaw(std::string raw)
+{
+
+}
+
+void MCInstance::say(std::string msg)
+{
+	sendRaw("say " + msg);
 }
--- minecraftd/MCInstance.hh
+++ minecraftd/MCInstance.hh
@@ -1,16 +1,16 @@
-/*
- * minecraftd/MCInstance.hh
- * 
- * Server instance management - Interface
- * 
- * Author: E. Mark Anderson <kd0bpv@gmail.com>
- * 
- * This file provides the interface for the Server Instance Management 
- * system which shall be responsible for monitoring and controlling 
+/**
+ * @file minecraftd/MCInstance.hh
+ *
+ * @brief Server instance management - Interface
+ *
+ * @author E. Mark Anderson <kd0bpv@gmail.com>
+ *
+ * This file provides the interface for the Server Instance Management
+ * system which shall be responsible for monitoring and controlling
  * individual Server.jar instances, and handle I/O to and from said instance.
- * 
+ *
  * Copyright (C) 2014-2015 E. Mark Anderson
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -27,53 +27,92 @@
 #define MCINSTANCE_H
 
 #include <string>
+#include <thread>
+#include <atomic>
+/* fstream derives from iostream, giving us both. :) */
+#include <fstream>
 
+#include "lib/event/libevent.hh"
 
+#ifndef FAIL_LIMIT
 #define FAIL_LIMIT 3
+#endif
 
-
-class FailEvent : public Event
-{
-public:
-	bool abort = false;
+enum class MCInstance::State {
+	STARTING, RUNNING,
+	STOPPING, STOPPED,
+	FAILED
 };
 
+struct MCInstance::Config
+{
+	/* Where am I? */
+	std::string InstancePath;
+
+	/* Who am I? */
+	std::string MCVersion;
+	std::string ForgeVersion = "none";
+	std::string ServerJar;
+
+	/* JVM Options */
+	std::string MinHeap = "512M";
+	std::string MaxHeap = "1G";
+	bool x64Mode = false; // Don't assume environment is 64-bit.
+	std::string MiscJVMArgs;
+
+	/* Minecraft Options */
+	bool OnlineMode = true;
+	std::string MiscMCArgs;
+};
+
+/* TODO: Probably should make fields Atomic. After all, I'm working with
+ * threads here...
+ */
 class MCInstance
 {
 public:
 	MCInstance();
-	State getState() {return state;}
-	int getFailCount() {return failCount;}
-	void resetFailState();
-	void doCommand(std::string cmd);
-	void say(std::string msg);
-	void run();
+	~MCInstance();
 
-	void start();
-	void stop(bool force = false); /* if things REALLY break, pass true to kill.
-				  This will most certainly cause data loss though. */
-	void restart() {stop(); start();}
-	enum class State {
-		STARTING, RUNNING,
-		STOPPING, STOPPED,
-		FAILED
-	};
-	
+	/* Inspection methods */
+	State getState() const noexcept {return state;}
+	int getFailCount() const noexcept {return failCount;}
+	void resetFailState() noexcept;
+
+	/* Management methods */
+	bool start();
+	/* If things REALLY break, pass true to kill.
+	 This will most certainly cause data loss though. */
+	bool stop(bool force = false);
+	bool restart() {stop(); return start();}
+
+	/* Input methods for server jar */
+	void sendRaw(std::string raw);
+	void say(std::string msg);
+
 	/* Events */
-	FailEvent onFail;
+	static Events::Event<MCInstance> FailEvent;
+
+	/* Operator Overloads */
+	MCInstance* operator<< (std::string raw); // "alias" for sendRaw()
 
 protected:
-	std::string instanceDir;
-	std::string serverJar;
-	bool isModded;
-	State state;
-	
+	std::thread runner;
+	void run(); // Wrapper for Server Jar. Run in thread.
+	std::atomic<Config> cfg;
+	std::atomic<State> state;
 	/* How many times this instance failed. Used to limit automatic restarts
 	 * which could cause excessive load.
 	 */
-	int failCount;
+	std::atomic<int> failCount;
+	std::atomic<int> pid;
 private:
-	
+
+};
+
+class MCForgeInstance : public MCInstance
+{
+
 };
 
 #endif
